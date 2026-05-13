c19948307b939a00fa43be58d8934b69a3f60549
Chisheng Chen <johnny1001s000602@gmail.com>
2026-05-13 20:48:43 +0800

    Update init script
    This does the following two changes:
    - print meminfo while boot
    - set environment variables

--- init/init.c
+++ init/init.c
@@ -3,21 +3,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define MOUNT_POINT "/root"
+
+static void print_meminfo() {
+    printf("\n--- System Memory Info (/proc/meminfo) ---\n");
+    FILE *fp = fopen("/proc/meminfo", "r");
+    if (fp) {
+        char line[256];
+        while (fgets(line, sizeof(line), fp))
+            printf("%s", line);
+
+        fclose(fp);
+        printf("------------------------------------------\n\n");
+    }
+}
+
 
 int main()
 {
     printf("Mounting rootfs\n");
     mount("proc", "/proc", "proc", 0, NULL);
-    while (mount("/dev/mmcblk0p1", "/usr", "ext2", 0, 0));
-
+    while (mount("/dev/mmcblk0p1", MOUNT_POINT, "ext2", 0, 0));
+    print_meminfo();
     char *argv[] = { "sh", NULL };
     char *envp[] = {
-        "PATH=/usr/bin",
-        "TSLIB_CALIBFILE=/usr/etc/pointercal",
-        "TSLIB_CONFFILE=/usr/etc/ts.conf",
+        "PATH=" MOUNT_POINT "/bin",
+        "HOME=" MOUNT_POINT,
+        "TSLIB_CALIBFILE=" MOUNT_POINT "/etc/pointercal",
+        "TSLIB_CONFFILE=" MOUNT_POINT "/etc/ts.conf",
         "TSLIB_TSDEVICE=/dev/input/event0",
+        "TSLIB_PLUGINDIR=/usr/lib/ts",
         "TSLIB_CONSOLEDEVICE=none",
         NULL
     };
-    execve("/usr/bin/sh", argv, envp);
+    execve(MOUNT_POINT "/bin/sh", argv, envp);
 }
